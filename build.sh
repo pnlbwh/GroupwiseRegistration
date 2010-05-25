33a6932f4e2a21efba1cc15619ac3ab7a5ac344b
reckbo <ryan.eckbo@gmail.com>
2010-05-25 17:01:19 -0400

    build.sh replaces make.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="slicer3-6/Slicer3/Applications/CLI/GroupWiseRegistration"
+
+if [ ! -e $dir ]; then
+  mkdir $dir
+  echo copying to $dir
+  cp GroupWiseRegistration/*  $dir
+  echo 'add_subdirectory(GroupWiseRegistration)' >> $dir/../CMakeLists.txt
+  slicer3-6/Slicer3/Scripts/getbuildtest.tcl -t "" --no-slicer-update
+else
+  echo copying to $dir
+  cp GroupWiseRegistration/*  $dir
+fi
+
+make -f slicer3-6/Slicer3-build/Applications/CLI/GroupWiseRegistration/Makefile
--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=slicer/Slicer3/Applications/CLI/GroupWiseRegistration/ 
-if [ -z $dir ]; then
-  mkdir $dir
-fi
-
-cp GroupWiseRegistration/*  $dir
-make -f slicer/Slicer3-build/Applications/CLI/GroupWiseRegistration/Makefile
