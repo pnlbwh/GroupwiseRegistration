ce40af03f4d646e72dc492c13e84555e18fba0bd
reckbo <reckbo@bwh.harvard.edu>
2011-09-02 10:05:16 -0400

    Moved tests and notes to their own directories

--- test0/runGroupWisePlugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATA_DIR='/net/lmi-terastation-b/mnt/array1/share/pnl/home/sylvain/data/ControlsAtlas/FAinAtlas'
-IMAGE1=$DATA_DIR/01183-Rgd-fa-Atlas.nii.gz
-IMAGE2=$DATA_DIR/01181-Rgd-fa-Atlas.nii.gz
-#../src/GroupWiseRegistration --volumeFileNames $DATA_DIR/G119_128.nrrd,$DATA_DIR/G132_128.nrrd --resultsDirectory ./ --num_multires_levels 5 --num_outer_iterations 5 --num_inner_iterations 5 --initial_sigma_diff 10 --final_sigma_diff 2 --reg_weight 10.0
-../src/GroupWiseRegistration --volumeFileNames $IMAGE1,$IMAGE2 --resultsDirectory ./ --num_multires_levels 5 --num_outer_iterations 5 --num_inner_iterations 5 --initial_sigma_diff 10 --final_sigma_diff 2 --reg_weight 10.0
--- test1/runGroupWisePlugin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/csh
-
-../../slicer3-6/Slicer3-build/lib/Slicer3/Plugins/GroupWiseRegistration --volumeFileNames ../G119_128.nrrd,../G132_128.nrrd,../G133_128.nrrd,../G136_128.nrrd,../G137_128.nrrd,../G139_128.nrrd --resultsDirectory ./ --output_image atlas.nii.gz --num_multires_levels 5 --num_outer_iterations 5 --num_inner_iterations 5 --initial_sigma_diff 10 --final_sigma_diff 2 --reg_weight 10.0
